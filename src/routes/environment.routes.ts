f83bb4208073e2795b9835275b86157c4f2f8340
Kalyan <vissamsettykalyan@gmail.com>
2026-03-10 19:27:23 +0530

    fix: change params for env delete

--- src/routes/environment.routes.ts
+++ src/routes/environment.routes.ts
@@ -40,7 +40,7 @@ router.get(
 
 // cascade delete env, versions
 router.delete(
-  "/environments/:envId",
+  "/:envId",
   requireAuth,
   requireRole([Role.ADMIN, Role.DEV]),
   deleteEnvironment,
